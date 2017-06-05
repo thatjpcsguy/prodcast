20ca0041f1326ecbeb0fdbc78d6a6c8c68547d83
James Cooper-Stanbury <jpcs@freelancer.com>
2017-06-05 21:47:01 +1000

    do a much more responsible job of loading the audio

--- prodcast.py
+++ prodcast.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, abort, send_file
+from flask import Flask, render_template, abort, send_file, request
 import postal
 import os
 
@@ -11,6 +11,7 @@ episodes = {
         'title': 'Dave Burson, Hotels Combined',
         'soundcloud-url': 'https://soundcloud.com/intercom/daniel-burka-design-partner-at',
         'soundcloud': '296867798',
+        'length': '28:28',
         'number': 1, 
         'url': 'dave-burson-hotels-combined'
     }, 
@@ -18,6 +19,7 @@ episodes = {
         'title': 'Carlos Pacheco, Escrow.com',
         'soundcloud-url': 'https://soundcloud.com/intercom/amy-thibodeau-content-strategy',
         'soundcloud': '325557698',
+        'length': '33:01',
         'number': 2,
         'url': 'carlos-pacheco-escrow'
     }
@@ -27,14 +29,14 @@ episodes = {
 @app.route('/')
 def index():
     # Change the 'latest' key to make the homepage show the latest episode
-    return render_template('homepage.html', title="Home", episodes=episodes, latest='carlos-pacheco-escrow')
+    return render_template('homepage.html', title="Home", episode='carlos-pacheco-escrow')
 
 
 @app.route('/episodes')
 def past_episodes():
     # TODO: sort the episodes into reverse order - currently they are "random"
     #       determined by the way python sorts its dictionary
-    return render_template('past.html', title="Past Episodes", episodes=episodes)
+    return render_template('past.html', title="Past Episodes")
 
 
 @app.route('/episodes/<name>')
@@ -43,7 +45,7 @@ def view_episode(name):
         return abort(404)
     # if not os.path.isfile('templates/episodes/%s.html'):
     #     return abort(404)
-    return render_template('episodes/%s.html' % name, episode=episodes[name], title=episodes[name]['title'])
+    return render_template('episodes/%s.html' % name, episode=name, title=episodes[name]['title'])
 
 
 @app.route('/sw.js')
@@ -55,6 +57,10 @@ def service_worker():
 def favicon():
     return send_file('static/img/favicon.ico')
 
+
+@app.context_processor
+def inject_url():
+    return dict(url=request.url_rule, episodes=episodes)
 
 # TODO: Send email to welcome people to the list
 # def send():
