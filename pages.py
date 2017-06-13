ab0b0eca9c3984ec64bbbb5baaebd9df00d3b95e
James Cooper-Stanbury <jpcs@freelancer.com>
2017-06-13 21:54:01 +1000

    small tweaks to the css - episode 1

--- pages.py
+++ pages.py
@@ -1,32 +1,17 @@
 # The main episode data store
 
-latest_episode = 'carlos-pacheco-escrow'
+latest_episode = 'welcome-to-the-prodcast'
 episodes = {
-    'dave-burson-hotels-combined': {
-        'title': 'Dave Burson, Hotels Combined',
-        'soundcloud-url': 'https://soundcloud.com/intercom/daniel-burka-design-partner-at',
-        'soundcloud': '296867794',
-        'length': '28:28',
-        'number': 1, 
-        'url': 'dave-burson-hotels-combined'
-    }, 
-    'carlos-pacheco-escrow': {
-        'title': 'Carlos Pacheco, Escrow.com',
-        'soundcloud-url': 'https://soundcloud.com/intercom/amy-thibodeau-content-strategy',
-        'soundcloud': '325557698',
-        'length': '33:01',
-        'number': 2,
-        'url': 'carlos-pacheco-escrow'
+    'welcome-to-the-prodcast': {
+        'title': 'Welcome to The Prodcast',
+        'length': '28:40',
+        'number': 1,
+        'url': 'welcome-to-the-prodcast'
     }
 }
 
 latest_article = 'what-to-do-when-your-startup-needs-program-management'
 articles = {
-    'the-web-is-repeating-itself': {
-        'title': 'The Web Is Repeating Itself',
-        'author': 'James',
-        'url': 'the-web-is-repeating-itself'
-    },
     'what-to-do-when-your-startup-needs-program-management': {
         'title': 'What to do when your startup needs program management',
         'author': 'Intercom',
