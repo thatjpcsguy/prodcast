ca5d205cf18eb71990587527d35e69f4e1a3dc33
James Cooper-Stanbury <jpcs@freelancer.com>
2017-06-20 01:06:46 +1000

    fixed social icons

--- pages.py
+++ pages.py
@@ -16,8 +16,18 @@ articles = {
     'lesson-focused-ab-testing': {
         'title': 'Lesson focused AB testing',
         'author': 'James Frankel',
+        'date': '2017-06-19',
+        'reading': '8 Minutes',
         'url': 'lesson-focused-ab-testing',
         'description': 'The 3 main reasons for testing are and why the third is the most overlooked, yet usually the most useful of all'
+    },
+    'the-ultimate-tools-for-gaining-user-insights': {
+        'title': 'The Ultimate Tools For Gaining User Insights',
+        'author': 'James Cooper-Stanbury',
+        'date': '2017-06-20',
+        'url': 'the-ultimate-tools-for-gaining-user-insights',
+        'reading': '5 Minutes',
+        'description': 'Gaining user insights is absolutely critical to sculpting a great product, so I\'ve put together this guide of some of the tools I\'ve personally used to gain insights into how my users use my products.'
     }
 }
 
